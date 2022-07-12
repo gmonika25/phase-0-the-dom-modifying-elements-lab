e35a5eec4d2983a1aaad0ab520abecfc0bcf9527
Monika Gustilo <monikagustilo@gmail.com>
2022-07-11 22:11:53 -0700

    commit tree 7a1632b184fcef99581d469e2bf57f6b5fc689e5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainID = document.getElementById("main");
+
+mainID.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "MONIKA is the champion";
